5a7748827f0989bdbeafa45c424dd018409ae70f
stanhaakman <stanhaakman@icloud.com>
2021-02-17 13:41:39 +0100

    Beter opdelen van files. Classes beter indelen en goed gebruik maken van de self. mogelijkheden

---
+++ classes/_data_recieve.py
@@ -0,0 +1,39 @@
+from pymongo import MongoClient
+
+
+class getdata(object):
+    """
+    Deze class is gemaakt voor het ophalen van data
+    geef een uitleg van elke functie in de vorm van een comment
+    """
+
+    def __init__(self, myclient, mydb):
+        self.myclient = myclient
+        self.mydb = mydb
+
+    def first_name_price(self):
+        # Geef de naam en de prijs van het eerste product in de lijst
+        return self.mydb.products().find_one({}, {"_id": 0, "name": 1, "price.selling_price": 1})
+
+    def name_start_letter(self, letter):
+        # Geef het eerste product terug, waar de naam begint met R
+        for i in self.mydb.products.find({}, {"_id": 0, "name": 1}):
+            var = i['name']
+            if letter == var[0]:
+                return var
+
+    def average_price(self):
+        totalprice = 0
+        totalitems = 0
+
+        for i in self.mydb.products.find({}, {"_id": 0, "price.selling_price": 1}):
+            if i != {}:
+                var = i["price"]
+                newvar = var["selling_price"]
+
+                # Pijzen die 0 zijn ook meenemen of overslaan.
+                # if newvar != 0:
+                totalprice += newvar
+                totalitems += 1
+
+        return totalprice / totalitems
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import pymongo
 from classes.tests._test_importMongodb import *
+from classes._data_recieve import getdata
 
-test_mongoDB()
+myclient = MongoClient("mongodb://localhost:27017/")
+mydb = myclient["huwebshop"]
+
+databaseData = getdata(myclient=myclient, mydb=mydb)
+
+print(databaseData.name_start_letter(letter='R'))
