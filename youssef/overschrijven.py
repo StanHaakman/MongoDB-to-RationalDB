3c01d845398df644ca46bdb33facb677bd9d298c
y0urselff <y.boulfiham@outlook.com>
2021-02-25 15:17:17 +0100

    connectie tussen python en postgres opgezet en data van mongodb naar csv opgezet

---
+++ youssef/overschrijven.py
@@ -0,0 +1,29 @@
+import pymongo
+from pymongo import MongoClient
+import csv
+
+myclient = MongoClient("mongodb://localhost:27017/")
+mydb = myclient["huwebshop"]
+
+
+def foo():
+
+    with open ('products.csv', 'w', newline='') as csvout:
+        fieldnames = ['id', 'brand', 'name']
+        writer = csv.DictWriter(csvout, fieldnames=fieldnames)
+        writer.writeheader()
+        c = 0
+        for product in mydb.products.find():
+            try:
+                if product['price']['selling_price'] != 0:
+                    writer.writerow({'id': product["_id"],
+                                     'brand': product.get('brand', None),
+                                     'name': product.get('name', None)})
+            except:
+                continue
+            c += 1
+            if c % 10000 == 0:
+                print('{} product records written...'.format(c))
+    print('Finished creating the product database content')
+
+foo()
