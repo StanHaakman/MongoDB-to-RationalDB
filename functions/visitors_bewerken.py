2c8da635de46fc0af2e31b6bbb17a675f57a832e
stanhaakman <stanhaakman@icloud.com>
2021-03-07 13:51:12 +0100

    Visitors bewerken id filteren oplossen

--- functions/visitors_bewerken.py
+++ functions/visitors_bewerken.py
@@ -1,4 +1,7 @@
 import math
+import random
+import time
+
 import pandas as pd
 
 
@@ -6,40 +9,20 @@ def id_filteren(dataframe):
     id = dataframe[['_id']]
     id_list = list(id._id)
     id_verbeterd = []
+    last_value = int
 
     for value in id_list:
-        value = ''.join(filter(str.isdigit, str(value)))
+        waarde = random.randint(0, 9)
+        while waarde == last_value:
+            waarde = random.randint(0, 9)
+
+        value = str(waarde).join(filter(str.isdigit, str(value)))
+        # time.sleep(0.1)
         id_verbeterd.append(value)
+        last_value = waarde
 
     dataframe._id = id_verbeterd
 
-    return dataframe
-
-
-def id_duplicates_old(dataframe):
-    c, d = 0, 0
-    id_verbeterd = []
-    for value in dataframe['_id']:
-        if value not in id_verbeterd:
-            id_verbeterd.append(value)
-        else:
-            d += 1
-            while True:
-                if c % 10000 == 1:
-                    print(f'{c} punten verwerkt')
-                if c in id_verbeterd:
-                    c += 1
-                    # print(f'nu {c}')
-                else:
-                    break
-            c += 1
-            id_verbeterd.append(c)
-    print(c, d)
-    return dataframe
-
-
-def id_duplicates(dataframe):
-    dataframe.drop_duplicates(subset=['_id'], keep='first', inplace=False)
     return dataframe
 
 
@@ -48,9 +31,6 @@ df = pd.read_csv('visitors.csv', encoding='utf-8')
 print('aanpassingen gestart, momentje!')
 df = id_filteren(df)
 
-# print(len(df.duplicated(subset=['_id'])))
-
-df = id_duplicates_old(df)
 
 df.to_csv('visitors.csv', index=False)
 print('processen beeindigd en opgeslagen!')
